150b5f8904b1cd8f259fd4d39152829a3eaebb96
dhanushmatcha <231fa04518@gmail.com>
2025-11-02 14:40:13 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,10 @@ const app = express();
 
 const PORT = process.env.PORT || 5000;
 const MONGO_URI = process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/birth';
-mongoose.connect(MONGO_URI)
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.log(err));
 app.use(express.json());
