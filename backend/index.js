3a1b71b2b10801517a85649e9d0c3cf378d69262
dhanushmatcha <231fa04518@gmail.com>
2025-11-02 13:28:33 +0530

    Fix duplicate PORT declaration

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,7 @@ const dotenv = require('dotenv');
 
 // Hardcoded Environment Variables
 const MONGO_URI = 'mongodb://127.0.0.1:27017/brith';
-const PORT = 5000;
+//const PORT = 5000;
 
 const app = express();
 
