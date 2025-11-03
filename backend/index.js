0ce6949809dab6241f11aec0fe9dff33eb94eb90
dhanushmatcha <231fa04518@gmail.com>
2025-11-03 14:45:58 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,7 @@ const app = express();
 
 // Environment Variables
 const PORT = process.env.PORT || 5000;
-const MONGO_URI = process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/birth';
+const MONGO_URI = process.env.MONGO_URI ;
 
 // Connect to MongoDB Atlas
 mongoose
